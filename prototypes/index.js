9331591cec6550e265b5a79c4128aab0aa8f8f72
Aidan McKay <aidanmckay2000@gmail.com>
2020-03-20 12:23:19 -0600

    Add test file and install npm

---
+++ prototypes/index.js
@@ -0,0 +1,39 @@
+const { kitties } = require('./datasets/kitties');
+const { clubs } = require('./datasets/clubs');
+const { mods } = require('./datasets/mods');
+const { cakes } = require('./datasets/cakes');
+const { classrooms } = require('./datasets/classrooms');
+const { breweries } = require('./datasets/breweries');
+const { instructors, cohorts } = require('./datasets/turing');
+const { bosses, sidekicks } = require('./datasets/bosses');
+const { constellations, stars } = require('./datasets/astronomy');
+const { weapons, characters } = require('./datasets/ultima');
+const { dinosaurs, humans, movies } = require('./datasets/dinosaurs');
+
+
+
+
+// Single Datasets 
+//  -------------------------------------
+//  -------------------------------------
+
+   // Return an array of just the names of kitties who are orange e.g.
+    // ['Tiger', 'Snickers']
+    const orangeKitten = () => {
+      return kitties.find(kitty.color === orange)
+    }
+
+     // Sort the kitties by their age
+
+      // Return an array of kitties who have all grown up by 2 years e.g.
+    // [{
+    //   name: 'Felicia',
+    //   age: 4,
+    //   color: 'grey'
+    // },
+    // {
+    //   name: 'Tiger',
+    //   age: 7,
+    //   color: 'orange'
+    // },
+    // ...etc]
